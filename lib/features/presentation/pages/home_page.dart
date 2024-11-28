91626191c417d4cf864fada69c2e654e23f71185
Willian Medeiros <willianmedeiroslm@gmail.com>
2024-11-28 11:11:14 -0400

    feature: function to hide the list

--- lib/features/presentation/pages/home_page.dart
+++ lib/features/presentation/pages/home_page.dart
@@ -12,12 +12,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
+  double opacityLevel = 1.0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void _changeOpacity() {
+    setState(() => opacityLevel = opacityLevel == 0 ? 1.0 : 0.0);
   }
 
   @override
@@ -27,24 +25,37 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: ListView.builder(
-        padding:
-            const EdgeInsets.only(bottom: 100, left: 16, right: 16, top: 0),
-        itemCount: characters.length,
-        itemBuilder: (context, index) {
-          final personagem = characters[index];
-          return PersonagemCardWidget(
-            name: personagem['name'],
-            race: personagem['race'],
-            url: personagem['url'],
-            strength: personagem['strength'],
-          );
-        },
+      body: AnimatedOpacity(
+        opacity: opacityLevel,
+        duration: const Duration(microseconds: 700),
+        child: ListView.builder(
+          padding:
+              const EdgeInsets.only(bottom: 100, left: 16, right: 16, top: 0),
+          itemCount: characters.length,
+          itemBuilder: (context, index) {
+            final personagem = characters[index];
+            return PersonagemCardWidget(
+              name: personagem['name'],
+              race: personagem['race'],
+              url: personagem['url'],
+              strength: personagem['strength'],
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        backgroundColor: const Color.fromARGB(255, 25, 149, 81),
+        onPressed: _changeOpacity,
+        tooltip: 'Opacity',
+        child: opacityLevel == 1.0
+            ? const Icon(
+                Icons.visibility,
+                color: Colors.white,
+              )
+            : const Icon(
+                Icons.visibility_off,
+                color: Colors.white,
+              ),
       ),
     );
   }
