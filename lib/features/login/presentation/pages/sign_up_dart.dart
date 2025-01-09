2af1a40add5197e1c3340214ff34d2c6f3962f3e
Willian Medeiros <willianmedeiroslm@gmail.com>
2025-01-09 09:31:59 -0400

    feature: spacing screen SignUpPage

--- lib/features/login/presentation/pages/sign_up_dart.dart
+++ lib/features/login/presentation/pages/sign_up_dart.dart
@@ -1,6 +1,7 @@
 import 'package:alura_quest/features/characterCreation/presentation/widgets/textForm_field_widget.dart';
 import 'package:alura_quest/features/login/presentation/widgets/line_separate_or_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -34,6 +35,16 @@ class _SignUpPageState extends State<SignUpPage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  SizedBox(
+                    height: 230,
+                    child: SvgPicture.asset(
+                      'assets/images/login.svg',
+                      semanticsLabel: 'Dart Logo',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
                   const Text(
                     "Criar conta",
                     style: TextStyle(
